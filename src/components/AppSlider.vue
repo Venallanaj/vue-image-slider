4ee98101256e265f9da2686e43646dc84207b1ca
Frontenddev123 <vena.llanaj@outlook.com>
2023-03-28 23:57:59 +0200

    adds

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <vue-carousel :per-page="1">
+      <vue-carousel-slide v-for="(slide, index) in slides" :key="index">
+        <div
+          class="slide"
+          :style="{ 'background-image': 'url(' + slide.image + ')' }"
+        >
+          <h2>{{ slide.title }}</h2>
+          <p>{{ slide.text }}</p>
+        </div>
+      </vue-carousel-slide>
+    </vue-carousel>
+  </div>
+</template>
+
+<script>
+import VueCarousel from "vue-carousel";
+
+export default {
+  name: "AppSlider",
+  components: {
+    VueCarousel,
+  },
+  data() {
+    return {
+      slides: [
+        {
+          image: "../assets/images/bg-1.jpg",
+          title: "Slide 1",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+          image: "/path/to/image2.jpg",
+          title: "Slide 2",
+          text: "Praesent euismod, augue quis bibendum pharetra, nisi turpis consectetur justo.",
+        },
+        {
+          image: "/path/to/image3.jpg",
+          title: "Slide 3",
+          text: "Sed in aliquet nulla. Nulla vel quam vitae velit euismod lobortis.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.slide {
+  height: 500px;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-align: center;
+}
+
+.slide h2 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.slide p {
+  font-size: 24px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <AppSlider />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import AppSlider from "@/components/AppSlider.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    AppSlider,
+  },
+};
 </script>
